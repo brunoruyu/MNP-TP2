9f4ccc4721cc5599504e889dc51d9a83afadb4b7
brunoruyu <brunoruyu@gmail.com>
2017-06-21 00:14:26 -0300

    Add files via upload

--- ProbDF.py
+++ ProbDF.py
@@ -5,6 +5,7 @@ Created on Sun Jun 18 16:08:12 2017
 @author: bruno
 """
 import numpy as np
+from itertools import tee, islice, chain
 
 def PDF(x,score,tipo,h):
     n=len(score)
@@ -19,10 +20,24 @@ def Kernel(u,tipo):
     elif tipo=='Linear':
         return max(1-np.abs(u),0)    
     elif tipo=='Epa':
-        return max(3*(1-u**2)/4,0)   
+        #return max(3/4*(1-(u**2)),0)   
+        return max(3/4*(1-(u**2)/5)/np.sqrt(5),0)   
     else:
         sys.exit('Elegir bien el Kernel')
     
         
 def SeqInt(f,X):
+    F=np.zeros(len(X))
+    for i in range(0,len(X)):
+        if i==0:
+            F[i]=0
+        else:
+            F[i]=F[i-1]+(X[i]-X[i-1])*(f[i]+f[i-1])/2
+    return F
+
+#def previous_and_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
+    prevs = chain([None], prevs)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
     
--- mainTP2.py
+++ mainTP2.py
@@ -11,38 +11,63 @@ import ReadTP2
 import ProbDF
 archivo="CustomerSample.dat"
 G,L,E='Gaussian','Linear','Epa'
-
+alfa=0.015
+N=700
 
 df,buenos,malos=ReadTP2.ReadCsv(archivo)
 segur=np.array(buenos['Z'])
 riesg=np.array(malos['Z'])
-score=np.array(df['Z'])
+todos=np.array(df['Z'])
 
-minZ=score.min()
-maxZ=score.max()
+minZ=todos.min()
+maxZ=todos.max()
 
-X=np.arange(minZ-50,maxZ+50,1)
-h=1.06*score.std()*len(score)**(-1/5)
+dx=0.05
+X=np.arange(minZ-2,maxZ+2,dx)
 
-print(h)
-f_tot=[ProbDF.PDF(x,score,G,h) for x in X] 
-f_seg=[ProbDF.PDF(x,segur,G,h) for x in X] #Gaussian, Linear o Epa
-f_riesg=[ProbDF.PDF(x,riesg,G,h) for x in X]
-         
-P_tot=[np.trapz(f_tot,x) for x in X]         
-#P_tot=[np.trapz(f_tot,x) for x in X]         
+file_write = open('ResultadosTP2.dat', 'w')    
+hopt=1.06*todos.std()*len(todos)**(-1/5)
+hh=[0.05,0.1,0.25,0.5,1,1.5,2,3]
+#hh=[1]
+for ke in [L,G,E]:#[L,G,E]:
+    for h in hh:
+        #h=
+        #ke=L   
+        print('h=',h,'Kernel=',ke)
+        
+        f_tot=[ProbDF.PDF(x,todos,ke,h) for x in X] 
+        f_seg=[ProbDF.PDF(x,segur,ke,h) for x in X] #Gaussian, Linear o Epa
+        f_riesg=[ProbDF.PDF(x,riesg,ke,h) for x in X]
+                 
+        P_tot=np.trapz(f_tot,X) 
+        #P_tot=[np.trapz(f_tot,x) for x in X]         
+        F_tot=ProbDF.SeqInt(f_tot,X)
+        F_seg=ProbDF.SeqInt(f_seg,X)
+        F_riesg=ProbDF.SeqInt(f_riesg,X)
+        
+        cociente=F_riesg/F_tot
+        z1=np.argmax(cociente>6*alfa)-1
+        
+        maxim=700*5/6*(F_seg-2*F_riesg)       
+        z2=np.argmax(maxim)
+        KS=max(np.abs(F_seg-F_riesg))
+        print('z1=',X[z1])
+        print('z2=',X[z2])
+        
+        string = ke+' '+str(h)+' '+str(X[z1])+' '+str(X[z2])+' '+str(KS)+ "\n"
+        #print(string)
+        file_write.write(string)
+        
+        #print(F_seg-F_riesg)
+file_write.close()
+      
 
-print(P_tot)
-         
-#plt.plot(X,f_seg,label='Seguros')
-#plt.plot(X,f_riesg,label='Riesgosos')
-
-plt.plot(X,f_tot,label='Todos')
+fig,ax1 = plt.subplots()
+plt.plot(X,f_seg,label='Seguros')
+plt.plot(X,f_riesg,label='Riesgosos')
+#plt.plot(X,cociente,label='cociente')
 plt.legend(loc=0)
-
-#from sklearn.neighbors.kde import KernelDensity
-#X = np.array([[])
-#kde = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(X)
-#kde.score_samples(X)
-
+#ax2=ax1.twinx()
+#plt.plot(X,maxim,'y',label='maxim')
+#plt.legend(loc=1)
 
